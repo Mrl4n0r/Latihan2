dec6e054bcf6e1260ad12b93353b61bc34100455
Mrl4n0r <lanamau368@gmail.com>
2025-06-12 12:17:34 +0800

    Membuat Objek

---
+++ DataDiriBeraksi.java
@@ -0,0 +1,11 @@
+package Latihan2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek
+        DataDiri maul = new DataDiri("Muhammad Maulana", "2310010375", "4C");
+
+        
+        maul.tampilkanData();
+    }
+}
